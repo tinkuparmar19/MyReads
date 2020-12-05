2beb928196f76ac77bb00bb538644970c86eac9f
tinkuparmar19 <tinkuparmar99@gmail.com>
2020-12-05 21:00:19 +0530

    delete yarn.lock

---
+++ build/precache-manifest.b667b39c45fc6a4dc625a7f59f3bc3ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "389ee9d28e74a70caa9b",
+    "url": "/static/js/main.389ee9d2.chunk.js"
+  },
+  {
+    "revision": "832b420eb1e06a53539c",
+    "url": "/static/js/1.832b420e.chunk.js"
+  },
+  {
+    "revision": "389ee9d28e74a70caa9b",
+    "url": "/static/css/main.de4629aa.chunk.css"
+  },
+  {
+    "revision": "ae87a6203296efcc6de7e722e14e4b94",
+    "url": "/index.html"
+  }
+];
